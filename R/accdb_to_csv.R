27f6531dbfd864a67cddbc48dbbbd59164345bbf
Jonathan Kennel <jonathankennel@pop-os.localdomain>
2020-08-31 19:44:42 -0400

    working

---
+++ R/accdb_to_csv.R
@@ -0,0 +1,42 @@
+#' accdb_to_csv
+#'
+#' @param accdb_file path to access database
+#' @param ... arguments to pass to fwrite
+#'
+#' @return
+#' @export
+accdb_to_csv <- function(accdb_file, ...) {
+  jackcess_loc <- file.path(system.file('', package = 'accdbexport'), "jackcess-3.5.0.jar")
+  logging_loc  <- file.path(system.file('', package = 'accdbexport'), "commons-logging-1.2.jar")
+  common_loc   <- file.path(system.file('', package = 'accdbexport'), "commons-lang3-3.11.jar")
+
+  rJava::.jinit()
+
+  rJava::.jaddClassPath(jackcess_loc)
+  rJava::.jaddClassPath(common_loc)
+  rJava::.jaddClassPath(logging_loc)
+
+  dbb     <- rJava::.jnew("com/healthmarketscience/jackcess/DatabaseBuilder")
+  dbjfile <- rJava::.jnew('java/io/File', accdb_file)
+  dbop    <- rJava::J(dbb, "open", dbjfile)
+
+
+  db_tbls  <- rJava::J(dbop, "getTableNames")
+  tbl_name <- rJava::J(db_tbls, "toString")
+  tbl_name <- gsub("[", '', tbl_name, fixed = TRUE)
+  tbl_name <- gsub("]", '', tbl_name, fixed = TRUE)
+  tbl_name <- strsplit(tbl_name, ', ')[[1]]
+
+  for(i in seq_along(tbl_name)) {
+
+    do_export <- !rJava::J(dbop, "getTableMetaData", tbl_name[i])$isLinked()
+    if(do_export) {
+
+      tabres <- rJava::J(dbop, "getTable", tbl_name[i])
+
+      fwrite(fread(rJava::J(tabres, "display")), paste0(tbl_name[i], '.csv'), ...)
+    }
+  }
+
+
+}
