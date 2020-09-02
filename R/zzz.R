797c0bc82235a338ef6be7b0e4d145b171aa46b0
Jonathan Kennel <jonathankennel@pop-os.localdomain>
2020-09-02 08:47:03 -0400

    fix travis

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,4 @@
   options(java.parameters="-Xrs")  ### so sun java does not kill R on CTRL-C
   rJava::.jpackage(pkgname, lib.loc = libname)
 }
-rJava::.jinit()
-jv <- rJava::.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-if(substr(jv, 1L, 1L) == "1") {
-  jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
-  if(jvn < 1.5) stop("Java 5 or higher is needed for this package but not available")
-}6
+
